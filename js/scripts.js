cf59f2c66704889d526e9ad320ff38eba4e52d97
LeiKD <dkomen180@gmail.com>
2020-03-03 11:37:03 +0300

    Add toggle function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function book() {
+function order() {
   var name= document.getElementById("name").value;
   var email= document.getElementById("email").value;
   var service= document.getElementById("service").value;
@@ -7,6 +7,17 @@ function book() {
   if (name==="" || email==="" || date==="" || time==="") {
     alert("Please fill in the necessary information before proceeding!")
   } else {
-  alert("Congratulations" + name + "!" + " You have successfully booked a " + service + " appointment with Haba KiBarber on " + date + ", at exactly " + time + "." + "We are glad that you chose us and we promise to provide the best service possible. Thank you!")
+  alert("Congratulations " + name + "!" + " You have successfully booked an appointment with Haba KiBarber. We are glad that you chose us and we promise to provide the best service possible. Thank you!")
   }
+  document.getElementById("x").innerHTML= "You have booked a " + service + " appointment."
+  document.getElementById("y").innerHTML= "We will be glad to offer you the selected service on " + date
+  document.getElementById("z").innerHTML= "Your appointment will be at exactly " + time 
+  document.getElementById("ad").innerHTML= "Please ensure that you arrive on the scheduled time of service. Until then, cheers!"
 }
+
+$(document).ready(function() {
+  $(".book").click(function() {
+    $("#summary").slideToggle()
+    $("#contact").slideToggle()
+  })
+})
